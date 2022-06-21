b62fc667b5274b24956849b1ed844a8de7cb3342
Burak Can Yurdacan <burakcanyurdacan@outlook.com>
2022-06-21 23:02:04 +0300

    4.gün

---
+++ OOP1/Program.cs
@@ -0,0 +1,22 @@
+﻿using OOP1;
+
+Product product1 = new Product();
+product1.ID = 1;
+product1.CategoryId = 2; //Mobilya olarak düşünelim
+product1.ProductName = "Masa";
+product1.UnitInStock = 6;
+product1.UnitPrice = 350;
+
+Product product2 = new Product
+{
+    ID = 2,
+    CategoryId = 3, //Kırtasiye olarak düşünelim
+    UnitPrice = 35,
+    ProductName = "Kalem",
+    UnitInStock = 6
+};
+
+ProductManager productManager = new ProductManager();
+productManager.Add(product2);
+
+Console.WriteLine(product2.ProductName);
